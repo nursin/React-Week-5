6df6ab8e9ef59c10b8904ccfec3de0e84cf44149
Bobby Keel <bobbykeel76@gmail.com>
2021-07-05 13:03:48 -0400

    First commit

---
+++ src/components/MainComponent.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import Directory from './DirectoryComponent';
+import Header from './HeaderComponent';
+import Footer from './FooterComponent';
+import Home from './HomeComponent';
+import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+import Contact from './ContactComponent';
+import About from './AboutComponent';
+import CampsiteInfo from './CampsiteInfoComponent';
+
+
+const mapStateToProps = state => {
+  return {
+    campsites: state.campsites,
+    comments: state.comments,
+    partners: state.partners,
+    promotions: state.promotions
+  }
+}
+
+class Main extends Component {
+
+  render() {
+      const HomePage = () => {
+        return (
+          <Home 
+            campsite={this.props.campsites.filter(campsite => campsite.featured)[0]}
+            promotion={this.props.promotions.filter(promotion => promotion.featured)[0]}
+            partner={this.props.partners.filter(partner => partner.featured)[0]}
+          />
+        );
+      }
+
+      const AboutPage = () => {
+        return (
+          <About partners={this.props.partners} />
+        );
+      }
+
+      const CampsiteWithId = ({match}) => {
+        return(
+          <CampsiteInfo campsite={this.props.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]} comments={this.props.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)} />
+        );
+      }
+
+      return (
+          <div>
+            <Header/>
+              <Switch>
+                <Route path='/home' component={HomePage} />
+                <Route exact path='/directory' render={() => <Directory campsites={this.props.campsites} />} />
+                <Route path='/directory/:campsiteId' component={CampsiteWithId} />
+                <Route path='/aboutus' component={AboutPage} />
+                <Route exact path='/contactus' component={Contact} />
+                <Redirect to='/home' />
+              </Switch>
+            <Footer />
+          </div>
+      );
+  }
+}
+
+export default connect(mapStateToProps)(Main);
