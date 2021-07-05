23fdbf3714ab87d493372e03faab40748ab70c55
Bobby Keel <bobbykeel76@gmail.com>
2021-07-05 13:53:04 -0400

    deleted reducer.js, added combinedReducer function and corresponding files campsites, ...

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,10 +1,18 @@
-import { createStore } from 'redux';
-import { Reducer, initialState } from './reducer';
+import { createStore, combineReducers } from 'redux';
+import { Campsites } from './campsites';
+import { Partners } from './partners';
+import { Promotions } from './promotions';
+import { Comments } from './comments';
+
 
 export const ConfigureStore = () => {
     const store = createStore(
-        Reducer,
-        initialState
+        combineReducers({
+          campsites: Campsites,
+          partners: Partners,
+          comments: Comments,
+          promotions: Promotions
+        })
     );
 
     return store;
